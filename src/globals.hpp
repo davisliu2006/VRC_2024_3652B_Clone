290daafb60a0d317ebc209e2451c4371a0232f5a
Davis Liu <davisliu2006@gmail.com>
2023-10-23 09:20:56 -0400

    Initialized library

---
+++ src/globals.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../include/main.h"
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+
+#include "headers/custom_math.hpp"
+#include "headers/geometry.hpp"
+
+using namespace std;
+
+// SETTINGS
+
+// drivetrain types
+#define TANK_DRV 1
+#define X_DRV 2
+// movable part types
+#define TYPE_MTR 1
+#define TYPE_PNEU 2
